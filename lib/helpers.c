1d19685635e58fd86f9ec6911a5c42d1cb6fe1cf
Alexey Katsman <alex2007508@gmail.com>
2015-03-17 21:22:54 +0300

    read_until added

--- lib/helpers.c
+++ lib/helpers.c
@@ -39,3 +39,28 @@ ssize_t write_(int fd, const void* buf, size_t count)
 		count -= have;
 	}
 }
+
+ssize_t read_until(int fd, void* buf, size_t count, char delimeter)
+{
+    int have = 0;
+    int res = 0;
+
+    while (1)
+    {
+        have = read(fd, (char*) buf + res, count);
+
+        for (int i = 0; i < have; i++)
+            if (((char*) buf)[res + i] == delimeter)
+                return have + res;
+
+        if (have == 0)
+            return res;
+        else if (have == -1)
+            return -1;
+        else if (have == count)
+            return have + res;
+
+        res += have;
+        count -= have;
+    }
+}
